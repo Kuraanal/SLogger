a9727185e4043c9695d0157e3b6efadb6774199b
Kuraanal <eric.agnel@gmail.com>
2023-05-02 02:36:07 +0800

    Replace date() function with DateTime and DateTimeZone objects.
    New optional parameter added to constructor $timeZone => Take a string representing one of the php Time Zones.
    
    IF the timwzone is valid, it will be used to log messages.
    If not valid, it will default to const DEFAULT_TIME_ZONE (Asia/Taipei).

--- Kuran/SLOGGER/Formaters/LineFormater.php
+++ Kuran/SLOGGER/Formaters/LineFormater.php
@@ -3,28 +3,37 @@ namespace Kuran\SLogger\Formaters;
 
 use Kuran\SLogger\ErrorLevel;
 use Kuran\SLogger\Formaters\FormaterInterface;
+use DateTime;
+use DateTimeZone;
 
 class LineFormater implements FormaterInterface
 {
 
     private const DEFAULT_STRING_FORMAT = "{:date} [{:errorLevel}] > {:message} (Context: {:context})\n";
     private const DEFAULT_TIME_FORMAT = "Y-m-d H:i:s";
+    private const DEFAULT_TIME_ZONE = "Asia/Taipei";
 
     private string $messageFormat;
     private string $timeFormat;
+    private DateTimeZone $timeZone;
 
-    public function __construct(string $format = null, string $timeFormat = null)
+    public function __construct(string $format = null, string $timeFormat = null, string $timeZone = null)
     {
         $this->messageFormat = $format === null ? static::DEFAULT_STRING_FORMAT : $format;
         $this->timeFormat = $timeFormat === null ? static::DEFAULT_TIME_FORMAT : $timeFormat;
+
+        $this->timeZone = $timeZone === null
+            ? new DateTimeZone(static::DEFAULT_TIME_ZONE)
+            : (in_array($timeZone, DateTimeZone::listIdentifiers()) ? new DateTimeZone($timeZone) : new DateTimeZone(static::DEFAULT_TIME_ZONE));
     }
 
     public function format(ErrorLevel $level, string $message, array $context): string
     {
         $decripted = $this->interpolate($message, $context);
+        $date = new DateTime('now', $this->timeZone);
 
         return $this->interpolate($this->messageFormat, array(
-            ":date" => date($this->timeFormat),
+            ":date" => $date->format($this->timeFormat),
             ":errorLevel" => $level->name,
             ":message" => $decripted,
             ":context" => json_encode($context),
